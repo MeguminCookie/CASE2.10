45fc7a12a596c9052ea98cf08c775938c75f5376
Xigmos <jonas.halttunen@outlook.com>
2023-11-20 11:30:18 +0100

    Aim assist

--- Assets/Runtime/EnemyHealth.cs
+++ Assets/Runtime/EnemyHealth.cs
@@ -9,16 +9,16 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] private float timeToAdd;
     public int maxHealth = 100; // You can set this value in the Unity Editor
 
-    private int currentHealth;
+    public int currentHealth;
 
-    private GameObject countDownObject;
-    private CountdownTimer timer;
+    //private GameObject countDownObject;
+    //private CountdownTimer timer;
 
     private void Start()
     {
         currentHealth = maxHealth;
-        countDownObject = GameObject.FindGameObjectWithTag("Gamemanager");
-        timer = countDownObject.GetComponent< CountdownTimer >(); 
+        //countDownObject = GameObject.FindGameObjectWithTag("Gamemanager");
+        //timer = countDownObject.GetComponent< CountdownTimer >(); 
     }
 
     public void TakeDamage(int damage)
@@ -28,7 +28,7 @@ public class EnemyHealth : MonoBehaviour
         // Check if the enemy is dead
         if (currentHealth <= 0)
         {
-            timer.TimeAdder(timeToAdd);
+            //timer.TimeAdder(timeToAdd);
             Die();
         }
     }
@@ -42,6 +42,7 @@ public class EnemyHealth : MonoBehaviour
     private void Die()
     {
         // Handle enemy death (e.g., play death animation, spawn particles, etc.)
-        Destroy(gameObject); // Destroy the enemy GameObject
+        //Destroy(gameObject); // Destroy the enemy GameObject
+        Debug.Log("Enemy killed!");
     }
 }
