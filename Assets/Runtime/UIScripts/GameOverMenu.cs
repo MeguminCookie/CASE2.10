9dac1db295301087449adf8b70f8b5db6d5421ab
Luka003-art <luka.vanden.poel@telenet.be>
2023-11-20 16:05:52 +0100

    StartScene Basic UI + Game Over Scene
    :)

---
+++ Assets/Runtime/UIScripts/GameOverMenu.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameOverMenu : MonoBehaviour
+{
+    [SerializeField] private float timeBetweenCounting = 0.01f;
+    [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private TextMeshProUGUI killsText;
+
+    [SerializeField] private int tempTimer;
+    [SerializeField] private int tempKills;
+
+    private float totalTime;
+    private float totalEnemiesKilled;
+
+    private int secTime, minTime, enemyCount;
+
+
+
+    void Start()
+    {
+        totalTime = PlayerPrefs.GetInt("Total Time");
+        totalEnemiesKilled = PlayerPrefs.GetInt("Killed Enemies");
+
+        totalTime = tempTimer;
+        totalEnemiesKilled = tempKills;
+
+        StartCoroutine(CountUpTimer());
+    }
+
+    private void Update()
+    {
+        if (minTime < 10)
+        {
+            if (secTime < 10)
+            {
+                timeText.SetText($"0{minTime}:0{secTime}");
+            }
+            else
+            {
+                timeText.SetText($"0{minTime}:{secTime}");
+            }
+        }
+        else
+        {
+            timeText.SetText($"{minTime}:{secTime}");
+        }
+
+        if (enemyCount > 9)
+        {
+            if (enemyCount > 99)
+            {
+                if (enemyCount > 999)
+                {
+                    if (enemyCount > 9999)
+                    {
+                        killsText.SetText($"{enemyCount}");
+                    }
+                    else
+                    {
+                        killsText.SetText($"0{enemyCount}");
+                    }
+                }
+                else
+                {
+                    killsText.SetText($"00{enemyCount}");
+                }
+            }
+            else
+            {
+                killsText.SetText($"000{enemyCount}");
+            }
+        }
+        else
+        {
+            killsText.SetText($"0000{enemyCount}");
+        }
+    }
+
+    private IEnumerator CountUpTimer()
+    {
+        for (int i = 0; i < totalTime; i++)
+        {
+            secTime++;
+            if (secTime == 60)
+            {
+                minTime++;
+                secTime = 0;
+            }
+
+            yield return new WaitForSeconds(timeBetweenCounting);
+        }
+        yield return new WaitForSeconds(0.3f);
+        StartCoroutine(CountUpKills());
+    }
+
+    private IEnumerator CountUpKills()
+    {
+        for (int i = 0; i < totalEnemiesKilled; i++)
+        {
+            enemyCount++;
+            yield return new WaitForSeconds(timeBetweenCounting);
+        }
+    }
+}
---
+++ Assets/Runtime/UIScripts/StartMenu.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    [Header("OtherPanels")]
+    [SerializeField] private GameObject highscorePanel;
+    [SerializeField] private GameObject credistPanel;
+
+    [Header("Buttons")]
+    [SerializeField] private GameObject button1;
+    [SerializeField] private GameObject button2;
+    [SerializeField] private GameObject button3;
+
+    private bool isOnStart;
+    private bool isOnHighScore;
+    private bool isOnCredits;
+
+    void Start()
+    {
+        button1.SetActive(true);
+        button2.SetActive(false);
+        button3.SetActive(false);
+
+        isOnStart = true;
+        isOnHighScore =  false;
+        isOnCredits = false;
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.lKey.wasPressedThisFrame)
+        {
+            if(isOnStart)
+            {
+                Debug.Log("Start Game");
+                //SceneManager.LoadScene(1);
+            }
+            if (isOnHighScore)
+            {
+                Debug.Log("Show Highscore");
+                /* highscorePanel.SetActive(true);
+                 * gameObject.SetActive(false);
+                 */
+            }
+            if (isOnCredits)
+            {
+                Debug.Log("Show Credits");
+                /* creditsPanel.SetActive(true);
+                 * gameObject.SetActive(false);
+                 */
+            }
+        }
+
+        if (Keyboard.current.downArrowKey.wasPressedThisFrame)
+        {
+            if (isOnStart)
+                StartCoroutine(SetToHighscore());
+            if (isOnHighScore)
+                StartCoroutine(SetToCedits());
+            if (isOnCredits)
+                StartCoroutine(SetToStart());
+        }
+
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+        {
+            if (isOnStart)
+                StartCoroutine(SetToCedits());
+            if (isOnHighScore)
+                StartCoroutine(SetToStart());
+            if (isOnCredits)
+                StartCoroutine(SetToHighscore());
+        }
+    }
+
+    private IEnumerator SetToStart()
+    {
+        button1.SetActive(true);
+        button2.SetActive(false);
+        button3.SetActive(false);
+
+        yield return new WaitForEndOfFrame();
+
+        isOnStart = true;
+        isOnHighScore = false;
+        isOnCredits = false;
+    }
+    
+    private IEnumerator SetToHighscore()
+    {
+        button1.SetActive(false);
+        button2.SetActive(true);
+        button3.SetActive(false);
+
+        yield return new WaitForEndOfFrame();
+
+        isOnStart = false;
+        isOnHighScore = true;
+        isOnCredits = false;
+    }
+
+    private IEnumerator SetToCedits()
+    {
+        button1.SetActive(false);
+        button2.SetActive(false);
+        button3.SetActive(true);
+
+        yield return new WaitForEndOfFrame();
+
+        isOnStart = false;
+        isOnHighScore = false;
+        isOnCredits = true;
+    }
+}
