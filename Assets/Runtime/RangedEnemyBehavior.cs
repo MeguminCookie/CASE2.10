ce202fbbf5a04292b77724990be6c032621e4cfa
Roy de Ridder <MeguminCookie@users.noreply.github.com>
2023-11-21 13:22:48 +0100

    useless

--- Assets/Runtime/RangedEnemyBehavior.cs
+++ Assets/Runtime/RangedEnemyBehavior.cs
@@ -53,78 +53,7 @@ public class RangedEnemyBehavior : MonoBehaviour
         //TryShootSmall(smallerWeaponTransform);
 
     }
-    /*private IEnumerator ShootTimerSmall()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(largerShootInterval);
-            canShootLarge = true;
-        }
-    }
-
-    private IEnumerator ShootTimerLarge()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(smallerShootInterval);
-            canShootLarge = true;
-        }
-    }
-
-    private void TryShootSmall(Transform smallerWeaponTransform)
-    {
-        if (canShootSmall && enemyFieldOfView.canSeePlayer)
-        {
-            // Instantiate a bullet at the weapon's position and rotation
-            GameObject bullet = Instantiate(smallBullet, smallerWeaponTransform.position, smallerWeaponTransform.rotation);
-
-            // Get the Rigidbody component of the bullet
-            Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
-
-            // Check if the bullet has a Rigidbody component
-            if (bulletRigidbody != null)
-            {
-                // Add force to the bullet in the forward direction of the largerWeaponTransform
-                bulletRigidbody.AddForce(smallerWeaponTransform.forward * bulletForce, ForceMode.Impulse);
-            }
-
-            canShootSmall = false;
-        }
-    }
-
-    private void TryShootLarge(Transform largerWeaponTransform)
-    {
-        if (canShootLarge && enemyFieldOfView.canSeePlayer)
-        {
-            // Instantiate a bullet at the weapon's position and rotation
-            GameObject bullet = Instantiate(largeBullet, largerWeaponTransform.position, largerWeaponTransform.rotation);
-
-            // Get the Rigidbody component of the bullet
-            Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
-
-            // Check if the bullet has a Rigidbody component
-            if (bulletRigidbody != null)
-            {
-                // Add force to the bullet in the forward direction of the largerWeaponTransform
-                bulletRigidbody.AddForce(largerWeaponTransform.forward * bulletForce, ForceMode.Impulse);
-            }
-
-            canShootLarge = false;
-        }
-    } */
-
-    /*
-    private bool HasLineOfSight()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, playerTransform.position - transform.position, out hit, range, raycastLayerMask))
-        {
-            return hit.collider.CompareTag("Player"); // Adjust the tag based on your player's tag
-        }
-        return false;
-    }
-    */
-
+    
     private IEnumerator MoveEnemy()
     {
         if (distance >= stopDistance)
@@ -145,14 +74,5 @@ public class RangedEnemyBehavior : MonoBehaviour
         yield return null;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("PlayerBullet"))
-        {
-            
-
-
-        }
-
-    }
+   
 }
