3b1ca51bcdb858d3029daf9c3249d052ca90f18b
Roy de Ridder <MeguminCookie@users.noreply.github.com>
2023-11-23 14:24:09 +0100

    Copywopy

--- Assets/Runtime/Soep/TimeAddSTextScript.cs
+++ Assets/Runtime/Soep/TimeAddSTextScript.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,12 +9,14 @@ public class TimeAddSTextScript : MonoBehaviour
     // Start is called before the first frame update
     private GameObject gamemanager;
     private CountdownTimer timer;
+    [SerializeField] private GameObject timeTextObject;
     [SerializeField] private TextMeshProUGUI timeText;
     void Start()
     {
         gamemanager = GameObject.FindGameObjectWithTag("GameManager");
         timer = gamemanager.GetComponent<CountdownTimer>();
-        timeText.SetText(timer.GetTimeDamage() + "s");
+        timeText.SetText( "-" +timer.GetTimeDamage() + "s");
+        StartCoroutine(Move());
     }
 
     // Update is called once per frame
@@ -25,7 +28,9 @@ public class TimeAddSTextScript : MonoBehaviour
 
     private IEnumerator Move()
     {
-        
+        timeText.transform.DOMoveY(timeText.transform.position.y + 1, 0.8f);
+        timeText.DOFade(1, 0.8f); 
         yield return new WaitForSeconds(0.8f);
+        Destroy(gameObject);
     }
 }
